20fc47a0aafa28db1b9562b8d3cb80596273a6aa
TomasSQ <tomasqueiroga@gmail.com>
2013-12-24 16:22:52 -0200

    debug

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-mvn clean install && mvn verify && cd view && mvn appengine:devserver -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y && cd ..
+mvn clean install && mvn verify && cd view && mvn appengine:devserver $1 && cd ..
