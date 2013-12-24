8a549163c36304503de6a4958b0058922292d6ce
TomasSQ <tomasqueiroga@gmail.com>
2013-12-24 19:25:02 -0200

    fonte certa, header mais baixo

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-/opt/appengine-java-sdk-1.8.8/bin/appcfg.sh update view/target/view-1.0.0-SNAPSHOT
+mvn clean install && mvn verify && /opt/appengine-java-sdk-1.8.8/bin/appcfg.sh update view/target/view-1.0.0-SNAPSHOT
