728ca786aeabe615a2407fb4692fd30ee17423fb
TomasSQ <tomasqueiroga@gmail.com>
2015-02-12 00:43:11 -0200

    sem java 8

--- business/src/main/java/br/com/tjgwp/domain/user/UserDomain.java
+++ business/src/main/java/br/com/tjgwp/domain/user/UserDomain.java
@@ -1,12 +1,12 @@
 package br.com.tjgwp.domain.user;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import br.com.tjgwp.business.entity.user.UserEntity;
 import br.com.tjgwp.business.entity.user.UserHistory;
 import br.com.tjgwp.domain.SuperDomain;
+
+import com.google.appengine.repackaged.org.joda.time.DateTime;
 
 public class UserDomain extends SuperDomain {
 
@@ -15,7 +15,7 @@ public class UserDomain extends SuperDomain {
 	}
 
 	public List<UserHistory> getLastedHistoryFromUser(UserEntity user) {
-		return ofy().load().type(UserHistory.class).limit(10).filter("date >", Instant.now().minus(10, ChronoUnit.DAYS).toEpochMilli()).ancestor(user).list();
+		return ofy().load().type(UserHistory.class).limit(10).filter("date >", DateTime.now().minusDays(10).getMillis()).ancestor(user).list();
 	}
 
 }
